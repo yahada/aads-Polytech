647d47c4320a713023e401a1c296fc164f12fcd2
yahada <niadzy14@gmail.com>
2026-02-16 16:21:44 +0300

    initial

---
+++ prac/prac_16_02_26/task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+template< class T >
+T* insert(const T* a, size_t s, size_t i, T v) // конструктор копирования
+{
+  T * r = new T[s + 1]; //1) T::T()
+  try {
+  for (size_t k = 0; k < s; ++k)
+  {
+    r[k] = a[k]; //1) T::operator=(T) 
+  }
+
+  r[i] = v; // (2)
+
+  for (size_t k = i + 1; k < s + 1; ++k)
+  {
+    r[k] = a[k - 1]; //(2)
+  }
+  } catch(...) {
+    delete[] r;
+    throw;
+  }
+  return r;
+}
