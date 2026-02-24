3d6179f7a42dff943a708acd16d620a3f170fd14
yahada <niadzy14@gmail.com>
2026-02-24 10:13:49 +0300

    task1 done

---
+++ prac/prac_24_02_26/task1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+template< class T >
+struct List
+{
+  T val;
+  List< T >* next;
+};
+
+template< class T >
+size_t size(const List< T >* h)
+{
+  size_t counter = 0;
+  while (h)
+  {
+    ++counter;
+    h = h->next;
+  }
+  return counter;
+  
+}
+
+template< class T >
+void clear(const List< T >* h)
+{
+  while (h)
+  {
+   List< T > * n = h->next;
+   delete h;
+   h = n; 
+  }
+  
+}
